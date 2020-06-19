3ee12a93139cc9ef0c66a933c817ea69fc8b38c3
Matheus Roversi <matheus.roversi@ahgora.com.br>
2020-06-19 09:03:11 -0300

    update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e5ee40d22c0b8535701d723613efaabc.js"
+  "https://localhost:3002/precache-manifest.0f56b8f2f1ea543baba0e31d93e696ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://localhost:3002/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-/* import 'materialize-css/dist/css/materialize.min.css'
-import './global.css' */
 import App from './App'
 
 const elroot = document.getElementById('root')
