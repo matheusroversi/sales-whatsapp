68c56e9bf4f68f7fd5926c6832de571cd9de5801
Matheus Roversi <matheus.roversi@ahgora.com.br>
2020-06-20 00:14:45 -0300

    update

--- src/core/app/client/AppClient.js
+++ src/core/app/client/AppClient.js
@@ -1,6 +1,4 @@
 import Api from "../../util/client/Api";
-import categories from "../../../file/menu.categories.web.json";
-import products from "../../../file/menu.products.web.json";
 
 const API_SERVER_ADDRESS =
   process.env.API_SERVER_ADDRESS || "http://localhost:8080";
@@ -10,19 +8,16 @@ export default {
     Api.get(`${API_SERVER_ADDRESS}/auth/google`);
   },
   async getMe() {
-    return Api.get("/me");
+    return Api.get(`${API_SERVER_ADDRESS}/me`);
   },
   async getUser() {
-    return Api.get("/user", { withCredentials: true });
+    return Api.get(`${API_SERVER_ADDRESS}/user`, { withCredentials: true });
   },
   async getCategories() {
-    return Api.get("/categories");
-    /* return await new Promise((resolve, reject) => {
-			resolve(categories)
-		}) */
+    return Api.get(`${API_SERVER_ADDRESS}/categories`);
   },
   async getProducts() {
-    return Api.get("/products");
+    return Api.get(`${API_SERVER_ADDRESS}/products`);
     /* return await new Promise((resolve, reject) => {
 			resolve(products)
 		}) */
