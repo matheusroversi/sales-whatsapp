31d23a5d80269650198cdc41bc7b51f1882fe908
Matheus Roversi <matheus.roversi@ahgora.com.br>
2020-06-20 00:24:19 -0300

    update

--- src/core/app/client/AppClient.js
+++ src/core/app/client/AppClient.js
@@ -1,23 +1,23 @@
 import Api from "../../util/client/Api";
 
-const API_SERVER_ADDRESS =
+const SERVER_ADDRESS =
   process.env.API_SERVER_ADDRESS || "http://localhost:8080";
 
 export default {
   async getLogin() {
-    Api.get(`${API_SERVER_ADDRESS}/auth/google`);
+    Api.get(`${SERVER_ADDRESS}/auth/google`);
   },
   async getMe() {
-    return Api.get(`${API_SERVER_ADDRESS}/me`);
+    return Api.get(`${SERVER_ADDRESS}/me`);
   },
   async getUser() {
-    return Api.get(`${API_SERVER_ADDRESS}/user`, { withCredentials: true });
+    return Api.get(`${SERVER_ADDRESS}/user`, { withCredentials: true });
   },
   async getCategories() {
-    return Api.get(`${API_SERVER_ADDRESS}/categories`);
+    return Api.get(`${SERVER_ADDRESS}/categories`);
   },
   async getProducts() {
-    return Api.get(`${API_SERVER_ADDRESS}/products`);
+    return Api.get(`${SERVER_ADDRESS}/products`);
     /* return await new Promise((resolve, reject) => {
 			resolve(products)
 		}) */
--- src/core/util/client/Api.js
+++ src/core/util/client/Api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const SERVER_ADDRESS = process.env.API_SERVER_ADDRESS; // || "http://localhost:8080";
+const SERVER_ADDRESS =
+  process.env.API_SERVER_ADDRESS || "http://localhost:8080";
 
 export default axios.create({
   baseURL: SERVER_ADDRESS,
